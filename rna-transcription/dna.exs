5cb5ec8441f73ecbfb0631150c751bf6392d7b57
BangN <thebangnguyen@gmail.com>
2016-03-13 13:24:44 +1100

    solve rna transcription problems

--- rna-transcription/dna.exs
+++ rna-transcription/dna.exs
@@ -8,7 +8,18 @@ defmodule DNA do
   'UGAC'
   """
   @spec to_rna([char]) :: [char]
-  def to_rna(dna) do
+  @mapper %{G: 'C', C: 'G', T: 'A', A: 'U'}
 
+  def to_rna(dna) do
+    dna
+    |> Enum.map( fn char -> replace(IO.chardata_to_string([char])) end )
+    |> Enum.join
+    |> to_char_list
+  end
+
+  defp replace(char) do
+    {:ok, result} = Map.fetch(@mapper, String.to_atom(char))
+
+    result
   end
 end
